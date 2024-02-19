8e21823eb7a20c61c7af93c3694c1d56902463e5
Everton Fernandes Medeiros <139650183+efernandes535@users.noreply.github.com>
2024-02-19 19:21:09 -0300

    melhorias

--- controle/switch.php
+++ controle/switch.php
@@ -1,7 +1,7 @@
 <div class="titulo">Switch</div>
 
 <?php
-$categoria = 'Luxo';
+$categoria = 'Sedan';
 $preco = 0.0;
 $carro = '';
 
@@ -19,5 +19,30 @@ if ($categoria === 'Luxo') {
     $preco = 30000;
 }
 
-$precoFormatado = number_format($preco, 2, ',','.');
+$precoFormatado = number_format($preco, 2, ',', '.');
+echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
+
+$categoria = '???';
+switch (strtolower($categoria)) {
+    case 'luxo':
+        $carro = 'Mustang';
+        $preco = '250000';
+        break;
+    case 'suv':
+    case 'suv Básico':
+        $carro = 'Renegade';
+        $preco = '80000';
+        break;
+    case 'sedan':
+        $carro = 'Omega';
+        $preco = '50000';
+        break;
+    default:
+        $carro = 'Marea';
+        $preco = '30000';
+        break;
+}
+
+
+$precoFormatado = number_format($preco, 2, ',', '.');
 echo "<p>Carro: $carro<br>Preço: R$ $precoFormatado</p>";
